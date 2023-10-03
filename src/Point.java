7c63cda1e6fd492d583055c24bf5fae161c3f2b4
BT_3W2_17 <weijial2@nycstudents.net>
2023-10-03 11:27:05 -0400

    Added quadrant method

--- src/Point.java
+++ src/Point.java
@@ -37,8 +37,25 @@ public class Point {
     }
 
     //In work
-    public int quadrant() {
-        if (x>0)
+    public String quadrant() {
+        if (x==0 && y==0) {
+            return "origin";
+        } else if (x==0 || y==0) {
+            return "on an axis";
+        }
+        if (x>0 && y>0) {
+            return "I";
+        }
+        if (x<0 && y>0) {
+            return "II";
+        }
+        if (x<0 && y<0) {
+            return "III";
+        }
+        if (x>0 && y<0) {
+            return "IV";
+        }
+        return null;
     }
 
 }
